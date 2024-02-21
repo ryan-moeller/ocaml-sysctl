1ae743495508c7aa5d13fb31431a091f81ad96a9
Ryan Moeller <ryan-moeller@att.net>
2024-02-21 16:51:17 -0500

    Expand test coverage

--- test/test_sysctl.ml
+++ test/test_sysctl.ml
@@ -1,6 +1,6 @@
 let () =
   Sysctl.nametomib "kstat.zfs"
-  |> Sysctl.iter
+  |> Sysctl.iter_noskip
   |> Seq.map (fun mib ->
          (Sysctl.name mib, Sysctl.format mib, Sysctl.description mib))
   |> Seq.map (fun (name, (_kind, fmt), desc) ->
@@ -9,3 +9,20 @@ let () =
          | None -> (name, fmt, ""))
   |> Seq.iter (fun (name, fmt, desc) ->
          Printf.printf "%s [%s]: %s\n" name fmt desc)
+
+let () =
+  Sysctl.nametomib "vfs.zfs"
+  |> Sysctl.iter
+  |> Seq.map (fun mib ->
+         (Sysctl.name mib, Sysctl.format mib, Sysctl.description mib, Sysctl.get mib))
+  |> Seq.map (fun (name, (_kind, fmt), desc, value) ->
+         let v = Sysctl.string_of_ctlval value in
+         match desc with
+         | Some descr -> (name, fmt, descr, v)
+         | None -> (name, fmt, "", v))
+  |> Seq.iter (fun (name, fmt, desc, value) ->
+         Printf.printf "%s [%s]: %s = %s\n" name fmt desc value)
+
+let () = Sysctl.all |> Seq.length |> Printf.printf "all: %d mibs\n"
+
+let () = Sysctl.all_noskip |> Seq.length |> Printf.printf "all_noskip: %d mibs\n"
